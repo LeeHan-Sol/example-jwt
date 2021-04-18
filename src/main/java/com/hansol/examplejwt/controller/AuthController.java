84a00e089927dea589e1b5cee2456064b42e12c9
Hansol Lee <lhs@partdb.com>
2021-04-18 19:51:16 +0900

    Add: Token 발급 api 적용 @lhs

---
+++ src/main/java/com/hansol/examplejwt/controller/AuthController.java
@@ -0,0 +1,45 @@
+package com.hansol.examplejwt.controller;
+
+import com.hansol.examplejwt.dto.LoginDto;
+import com.hansol.examplejwt.dto.TokenDto;
+import com.hansol.examplejwt.jwt.JwtFilter;
+import com.hansol.examplejwt.jwt.TokenProvider;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api")
+public class AuthController {
+
+    private final TokenProvider tokenProvider;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+
+    public AuthController(TokenProvider tokenProvider, AuthenticationManagerBuilder authenticationManagerBuilder) {
+        this.tokenProvider = tokenProvider;
+        this.authenticationManagerBuilder = authenticationManagerBuilder;
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<TokenDto> authorize(@Valid @RequestBody LoginDto loginDto) {
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword());
+
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = tokenProvider.createToken(authentication);
+
+        return ResponseEntity.ok()
+                .header(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + jwt)
+                .body(new TokenDto(jwt));
+    }
+}
