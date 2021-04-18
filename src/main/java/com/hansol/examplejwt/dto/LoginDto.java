df8533cd67773c37e8f42665d144bd6fe5673061
Hansol Lee <lhs@partdb.com>
2021-04-18 18:56:42 +0900

    Add: Login, Token, User Dto 적용 @lhs

---
+++ src/main/java/com/hansol/examplejwt/dto/LoginDto.java
@@ -0,0 +1,29 @@
+package com.hansol.examplejwt.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class LoginDto {
+
+    @NotNull
+    @Size(min = 3, max = 50)
+    private String username;
+
+    @NotNull
+    @Size(min = 3, max = 100)
+    private String password;
+
+    @Builder
+    public LoginDto(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
